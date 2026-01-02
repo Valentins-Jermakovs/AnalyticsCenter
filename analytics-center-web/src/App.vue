366fcba7610a576a865191807b8b960983a1cd1c
Valentins Jermakovs <jermakovsvalentins@gmail.com>
2026-01-02 19:27:55 +0200

    Add Font Awesome

--- analytics-center-web/src/App.vue
+++ analytics-center-web/src/App.vue
@@ -2,6 +2,8 @@
 
 <template>
   <h1 class="text-4xl text-base-content">You did it!</h1>
+  <font-awesome-icon icon="fa-solid fa-house" />
+  <font-awesome-icon icon="fa-regular fa-circle-user" />
 </template>
 
 <style scoped></style>
